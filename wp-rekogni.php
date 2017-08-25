618ba226fef13ce31e455f3ca9eb072430a84d9f
mt8 <info@mt8.biz>
2017-08-25 22:55:37 +0900

    Fix uninstall hook

--- wp-rekogni.php
+++ wp-rekogni.php
@@ -18,21 +18,7 @@ require_once plugin_dir_path( __FILE__ ) . '/class-wp-rekogni-admin.php';
 $wp_rekogni = new WP_Rekogni();
 $wp_rekogni->register_hooks();
 
-
-function wp_rekogni_plugin_activate() {
-	$option_keys = WP_Rekogni::get_option_keys();
-	foreach ( $option_keys as $key ) {
-		if ( get_option( $key ) == '' ) {
-			update_option( $key, '' );
-		}
-	}
-}
-register_activation_hook( __FILE__, 'wp_rekogni_plugin_activate' );
-
 function wp_rekogni_plugin_uninstall() {
-	$option_keys = WP_Rekogni::get_option_keys();
-	foreach ( $option_keys as $key ) {
-		delete_option( $key );
-	}
+	delete_option( WP_Rekogni::SLUG );
 }
 register_uninstall_hook( __FILE__, 'wp_rekogni_plugin_uninstall' );
